ade02143fe3d60cf5ef7cf809a3eb900d7fa39e9
amir kamolov <kamolov.amir2000@yandex.ru>
2021-11-12 19:34:31 +0300

    [+] search

--- backend/catalog/filters.py
+++ backend/catalog/filters.py
@@ -7,7 +7,7 @@ class ProductFilter(django_filters.FilterSet):
     price = django_filters.RangeFilter()
     # искать не по категрии а по жанру
     genre = django_filters.CharFilter(field_name='values__value')
-
+    q = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     class Meta:
         model = Product
-        fields = ['price', 'genre']
+        fields = ['price', 'genre', 'q']
